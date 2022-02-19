f1e81874baaf8b5582f1fb073d34007a535abd01
Cory Tveten <corytveten@gmail.com>
2022-02-19 10:03:14 -0500

    reorg files

---
+++ js/cardData.js
@@ -0,0 +1,91 @@
+const getHeroes = () => [
+    { name: "Captain America", id: 1, set: "core"},
+    { name: "Black Widow", id: 2, set: "core"},
+    { name: "Hulk", id: 3, set: "core"},
+    { name: "Spider-Man", id: 4, set: "core"},
+    { name: "Wolverine", id: 5, set: "core"},
+    { name: "Cyclops", id: 6, set: "core"},
+    { name: "Nick Fury", id: 7, set: "core"},
+    { name: "Deadpool", id: 8, set: "core"},
+    { name: "Emma Frost", id: 9, set: "core"},
+    { name: "Gambit", id: 10, set: "core"},
+    { name: "Hawkeye", id: 11, set: "core"},
+    { name: "Rogue", id: 12, set: "core"},
+    { name: "Storm", id: 13, set: "core"},
+    { name: "Thor", id: 14, set: "core"},
+    { name: "Iron Man", id: 15, set: "core"},
+    { name: "Angel", id: 16, set: "dark-city"},
+    { name: "Bishop", id: 17, set: "dark-city"},
+    { name: "Colossus", id: 18, set: "dark-city"},
+    { name: "Daredevil", id: 19, set: "dark-city"},
+    { name: "Domino", id: 20, set: "dark-city"},
+    { name: "Elektra", id: 21, set: "dark-city"},
+    { name: "Forge", id: 22, set: "dark-city"},
+    { name: "Ghost Rider", id: 23, set: "dark-city"},
+    { name: "Iceman", id: 24, set: "dark-city"},
+    { name: "Iron Fist", id: 25, set: "dark-city"},
+    { name: "Jean Grey", id: 26, set: "dark-city"},
+    { name: "Nightcrawler", id: 27, set: "dark-city"},
+    { name: "Professor X", id: 28, set: "dark-city"},
+    { name: "Punisher", id: 29, set: "dark-city"},
+    { name: "Weapon X Wolverine", id: 30, set: "dark-city"},
+    { name: "Cable", id: 31, set: "dark-city"},
+    { name: "Blade", id: 32, set: "dark-city"},
+
+]
+
+const getMasterminds = () => [
+    { name: "Magneto", id: 1, set: "core"},
+    { name: "Dr. Doom", id: 2, set: "core"},
+    { name: "Red Skull", id: 3, set: "core"},
+    { name: "Loki", id: 4, set: "core"},
+    { name: "Apocalypse", id: 5, set: "dark-city"},
+    { name: "Kingpin", id: 6, set: "dark-city"},
+    { name: "Mephisto", id: 7, set: "dark-city"},
+    { name: "Mr. Sinister", id: 8, set: "dark-city"},
+    { name: "Stryfe", id: 9, set: "dark-city"},
+
+]
+
+const getVillains = () => [
+    { name: "Spider-foes", id: 1, set: "core" },
+    { name: "Brotherhood", id: 2, set: "core" },
+    { name: "Enemies of Asgard", id: 3, set: "core" },
+    { name: "HYDRA", id: 4, set: "core" },
+    { name: "Masters of Evil", id: 5, set: "core" },
+    { name: "Radiation", id: 6, set: "core" },
+    { name: "Skrulls", id: 7, set: "core" },
+    { name: "Emissaries of Evil", id: 8, set: "dark-city" },
+    { name: "Four Horsemen", id: 9, set: "dark-city" },
+    { name: "Marauders", id: 10, set: "dark-city" },
+    { name: "Mutant Liberation Front", id: 11, set: "dark-city" },
+    { name: "Streets of New York", id: 12, set: "dark-city" },
+    { name: "Underworld", id: 13, set: "dark-city" },
+
+]
+
+const getHenchmen = () => [
+    { name: "Doombot Legion", id: 1, set: "core"},
+    { name: "Hand Ninjas", id: 2, set: "core"},
+    { name: "Savage Land Mutates", id: 3, set: "core"},
+    { name: "Sentinel", id: 4, set: "core"},
+    { name: "Maggia Goons", id: 5, set: "dark-city" },
+    { name: "Phalanx", id: 6, set: "dark-city" },
+
+]
+
+const getScheme = () => [
+    { name: "Super Hero Civil War", id: 1, set: "core"},
+    { name: "Secret Invasion of the Skrull Shapeshifters", id: 2, set: "core"},
+    { name: "Replace Earth's Leaders with Killbots", id: 3, set: "core"},
+    { name: "The Legacy Virus", id: 4, set: "core"},
+    { name: "Midtown Bank Robbery", id: 5, set: "core"},
+    { name: "Capture Baby Hope", id: 6, set: "dark-city"},
+    { name: "X-Cutioner's Song", id: 7, set: "dark-city"},
+    { name: "Transform Citizens Into Demons", id: 8, set: "dark-city"},
+    { name: "Steal the Weaponized Plutonium", id: 9, set: "dark-city"},
+    { name: "Detonate the Helicarrier", id: 10, set: "dark-city"},
+    { name: "Save Humanity", id: 11, set: "dark-city"},
+    { name: "Massive Earthquake Generator", id: 12, set: "dark-city"},
+    { name: "Organized Crime Wave", id: 13, set: "dark-city"},
+]
